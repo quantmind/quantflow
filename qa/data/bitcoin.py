6d220851806378e1fb6215f85197876d16ba38b7
quantmind <luca.sbardella@citi.com>
2014-01-17 18:30:19 +0000

    1

--- qa/data/bitcoin.py
+++ qa/data/bitcoin.py
@@ -1,8 +1,40 @@
+from pulsar import in_loop
+from pulsar.apps import Application
+from pulsar.apps.http import HttpClient
+from pulsar.apps.ws import WS
 
 
-
-class BitCoin(DataFeed):
+class BitCoin(object):
     root_url = 'https://www.bitstamp.net/api/'
+
+    def __init__(self, http):
+        self.http = http
+
+    @property
+    def _loop(self):
+        return self.http._loop
 
     def ticker(self):
         return self.http.get(self.url('ticker/'))
+
+    @in_loop
+    def mtgox(self):
+        res =  yield self.http.get('ws://ws.blockchain.info/inv',
+                                   websocket_handler=MtGox())
+        a = res.get_content()
+        b = 1
+
+
+class MtGox(WS):
+
+    def on_message(self, websocket, message):
+        print(message)
+
+
+class Robot(Application):
+
+    def worker_start(self, worker):
+        http = HttpClient()
+        b = BitCoin(http)
+        b.mtgox()
+
