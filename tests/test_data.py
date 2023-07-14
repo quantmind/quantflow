054e4ed649e97c9ea26ef5871f945d5f383f91a1
Luca Sbardella <luca@quantmind.com>
2023-07-14 22:22:26 +0100

    use context manager

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,5 @@
+from typing import AsyncIterator, cast
+
 import pytest
 
 from quantflow.data.fmp import FMP
@@ -6,8 +8,9 @@ pytestmark = pytest.mark.skipif(FMP().key is None, reason="No FMP API key found"
 
 
 @pytest.fixture
-def fmp() -> FMP:
-    return FMP()
+async def fmp() -> AsyncIterator[FMP]:
+    async with FMP() as fmp:
+        yield cast(FMP, fmp)
 
 
 def test_client(fmp: FMP) -> None:
@@ -18,3 +21,8 @@ def test_client(fmp: FMP) -> None:
 async def test_historical(fmp: FMP) -> None:
     df = await fmp.prices("BTCUSD")
     assert df["close"] is not None
+
+
+async def test_dividends(fmp: FMP) -> None:
+    data = await fmp.dividends()
+    assert data is not None
