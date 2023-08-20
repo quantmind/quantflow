e8ea5737834de355633b4973fc018fb07fe6f386
Luca Sbardella <luca@quantmind.com>
2023-08-20 18:51:18 +0100

    Add copula

---
+++ tests/test_copula.py
@@ -0,0 +1,29 @@
+from math import isclose
+
+import numpy as np
+
+from quantflow.sp.copula import FrankCopula, IndependentCopula
+
+
+def test_independent_copula():
+    c = IndependentCopula()
+    assert c.tau() == 0
+    assert c.rho() == 0
+    assert np.allclose(c.jacobian(0.3, 0.4), np.array([0.4, 0.3]))
+
+
+def test_frank_copula():
+    c = FrankCopula(kappa=0.3)
+    assert c.kappa == 0.3
+    assert c.tau() > 0
+    assert c.rho() < 0
+    assert c.jacobian(0.3, 0.4).shape == (3,)
+
+    c.kappa = 0
+    assert c.tau() == 0
+    assert c.rho() == 0
+    assert np.allclose(c.jacobian(0.3, 0.4), np.array([0.4, 0.3, 0.0]))
+
+    c = FrankCopula()
+    assert isclose(c(11.0, 3.0), 33.0)
+    assert isclose(c(11.0, 3.0), 33.0)
