0947e680c2f7c2885a7648e27785c32f5611883c
Luca <luca@quantmind.com>
2025-01-01 14:19:25 +0000

    Holidays package for cli

--- quantflow/cli/script.py
+++ quantflow/cli/script.py
@@ -4,11 +4,11 @@ dotenv.load_dotenv()
 
 try:
     from .app import QfApp
-except ImportError:
+except ImportError as ex:
     raise ImportError(
         "Cannot run qf command line, "
         "quantflow needs to be installed with cli & data extras, "
         "pip install quantflow[cli, data]"
-    ) from None
+    ) from ex
 
 main = QfApp()
