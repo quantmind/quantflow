Fix heston with Jumps variance

quantflow/options/calibration.py
@@ -11,8 +11,9 @@ import pandas as pd
 from scipy.optimize import Bounds, OptimizeResult, minimize
 
 from quantflow.sp.base import StochasticProcess1D
-from quantflow.sp.heston import Heston
+from quantflow.sp.heston import Heston, HestonJ
 from quantflow.utils import plot
+from quantflow.utils.distributions import DoubleExponential
 
 from .pricer import OptionPricer
 from .surface import OptionPrice, VolSurface
@@ -123,7 +124,7 @@ class VolModelCalibration(ABC, Generic[M]):
         return replace(self, options=options)
 
     def get_bounds(self) -> Bounds | None:  # pragma: no cover
-        """Get the bounds for the calibration"""
+        """Get the parameter bounds for the calibration"""
         return None
 
     def get_constraints(self) -> Sequence[dict[str, Any]] | None:
@@ -225,6 +226,54 @@ class HestonCalibration(VolModelCalibration[Heston]):
         self.model.variance_process.kappa = params[2]
         self.model.variance_process.sigma = params[3]
         self.model.rho = params[4]
+
+    def penalize(self) -> float:
+        kt = 2 * self.model.variance_process.kappa * self.model.variance_process.theta
+        neg = max(0.5 * self.model.variance_process.sigma2 - kt, 0)
+        return self.feller_penalize * neg * neg
+
+
+@dataclass
+class HestonJCalibration(VolModelCalibration[HestonJ[DoubleExponential]]):
+    """A :class:`.VolModelCalibration` for the :class:`.HestonJ`
+    stochastic volatility model with :class:`.DoubleExponential`
+    jumps
+    """
+
+    feller_penalize: float = 0.0
+
+    def get_bounds(self) -> Sequence[Bounds] | None:
+        vol_range = self.implied_vol_range()
+        vol_lb = 0.5 * vol_range.lb[0]
+        vol_ub = 1.5 * vol_range.ub[0]
+        return Bounds(
+            [vol_lb * vol_lb, vol_lb * vol_lb, 0.0, 0.0, -0.9],
+            [vol_ub * vol_ub, vol_ub * vol_ub, np.inf, np.inf, 0],
+        )
+
+    def get_params(self) -> np.ndarray:
+        return np.asarray(
+            [
+                self.model.variance_process.rate,
+                self.model.variance_process.theta,
+                self.model.variance_process.kappa,
+                self.model.variance_process.sigma,
+                self.model.rho,
+                self.model.jumps.intensity,
+                self.model.jumps.jumps.decay,
+                self.model.jumps.jumps.log_kappa,
+            ]
+        )
+
+    def set_params(self, params: np.ndarray) -> None:
+        self.model.variance_process.rate = params[0]
+        self.model.variance_process.theta = params[1]
+        self.model.variance_process.kappa = params[2]
+        self.model.variance_process.sigma = params[3]
+        self.model.rho = params[4]
+        self.model.jumps.intensity = params[5]
+        self.model.jumps.jumps.decay = params[6]
+        self.model.jumps.jumps.kappa = np.exp(params[7])
 
     def penalize(self) -> float:
         kt = 2 * self.model.variance_process.kappa * self.model.variance_process.theta

quantflow/utils/distributions.py
@@ -114,10 +114,10 @@ class DoubleExponential(Exponential):
         \end{align}
 
     where `m` is the :attr:`.loc` parameter, :math:`\lambda` is the :attr:`.decay`
-    parameter, and :math:`\kappa` is the asymmetric :attr:`.k` parameter.
+    parameter, and :math:`\kappa` is the asymmetric :attr:`.kappa` parameter.
 
-    The Laplace distribution is similar to the Gaussian/normal distribution,
-    but is sharper at the peak and has fatter tails.
+    The Asymmetric Laplace distribution is similar to the Gaussian/normal distribution,
+    but is sharper at the peak, it has fatter tails and allow for skewness.
     It represents the difference between two independent, exponential random variables.
 
     .. _ALD: https://en.wikipedia.org/wiki/Asymmetric_Laplace_distribution
@@ -129,6 +129,11 @@ class DoubleExponential(Exponential):
     r"""The exponential decay rate :math:`\lambda`"""
     kappa: float = Field(default=1, gt=0, description="asymmetric parameter")
     """Asymmetric parameter - when k=1, the distribution is symmetric"""
+
+    @property
+    def log_kappa(self) -> float:
+        """The log of the :attr:`.kappa` parameter"""
+        return np.log(self.kappa)
 
     @classmethod
     def from_moments(