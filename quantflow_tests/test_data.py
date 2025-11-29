d18456ec1187762a0fa764f99893d81b2fc3e787
Luca Sbardella <luca@quantmind.com>
2025-11-29 17:27:46 +0000

    py 3.14 (#32)

--- quantflow_tests/test_data.py
+++ quantflow_tests/test_data.py
@@ -1,13 +1,13 @@
 from typing import AsyncIterator
 
 import pytest
-from aiohttp.client_exceptions import ClientError
 
 from quantflow.data.fed import FederalReserve
 from quantflow.data.fiscal_data import FiscalData
 from quantflow.data.fmp import FMP
+from quantflow_tests.utils import skip_network_issue
 
-pytestmark = pytest.mark.skipif(not FMP().key, reason="No FMP API key found")
+skip_fmp = not FMP().key
 
 
 @pytest.fixture
@@ -16,49 +16,46 @@ async def fmp() -> AsyncIterator[FMP]:
         yield fmp
 
 
+@pytest.mark.skipif(skip_fmp, reason="No FMP API key found")
 def test_client(fmp: FMP) -> None:
     assert fmp.url
     assert fmp.key
 
 
+@pytest.mark.skipif(skip_fmp, reason="No FMP API key found")
 async def test_historical(fmp: FMP) -> None:
     df = await fmp.prices("BTCUSD", fmp.freq.one_hour)
     assert df["close"] is not None
 
 
+@pytest.mark.skipif(skip_fmp, reason="No FMP API key found")
 async def test_dividends(fmp: FMP) -> None:
     data = await fmp.dividends()
     assert data is not None
 
 
+@skip_network_issue
 async def test_fed_yc() -> None:
-    try:
-        async with FederalReserve() as fed:
-            df = await fed.yield_curves()
-            assert df is not None
-            assert df.shape[0] > 0
-            assert df.shape[1] == 12
-    except (ConnectionError, ClientError) as e:
-        pytest.skip(f"Skipping test_fed due to network issue: {e}")
+    async with FederalReserve() as fed:
+        df = await fed.yield_curves()
+        assert df is not None
+        assert df.shape[0] > 0
+        assert df.shape[1] == 12
 
 
+@skip_network_issue
 async def test_fed_rates() -> None:
-    try:
-        async with FederalReserve() as fed:
-            df = await fed.ref_rates()
-            assert df is not None
-            assert df.shape[0] > 0
-            assert df.shape[1] == 2
-    except (ConnectionError, ClientError) as e:
-        pytest.skip(f"Skipping test_fed due to network issue: {e}")
+    async with FederalReserve() as fed:
+        df = await fed.ref_rates()
+        assert df is not None
+        assert df.shape[0] > 0
+        assert df.shape[1] == 2
 
 
+@skip_network_issue
 async def __test_fiscal_data() -> None:
-    try:
-        async with FiscalData() as fd:
-            df = await fd.securities()
-            assert df is not None
-            assert df.shape[0] > 0
-            assert df.shape[1] == 2
-    except (ConnectionError, ClientError) as e:
-        pytest.skip(f"Skipping test_fed due to network issue: {e}")
+    async with FiscalData() as fd:
+        df = await fd.securities()
+        assert df is not None
+        assert df.shape[0] > 0
+        assert df.shape[1] == 2
